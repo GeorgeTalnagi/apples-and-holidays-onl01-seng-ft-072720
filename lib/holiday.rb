15446d452db30fc8cb72ebf43d43af112aa29fc9
George Talnagi <gtalnagi@yahoo.com>
2020-08-03 19:01:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
-      puts "#{event.to_s.split("_").collect { |a| a.capitalize }.join(" ") }: #{supplies.join(", ")}"
+      puts "  #{event.to_s.split("_").collect { |a| a.capitalize }.join(" ") }: #{supplies.join(", ")}"
   end
 end
 end
