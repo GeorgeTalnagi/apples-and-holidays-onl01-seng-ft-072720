daf911693a0eaf2afacafa830e3c3849ad06f21c
George Talnagi <gtalnagi@yahoo.com>
2020-08-03 19:04:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.collect do |season, holidays|
+    holidays.collect do |event, supplies|
+      if supplies.include?("BBQ")
+        event 
+      end 
+    end
+  end.flatten.compact 
 
 end
 
