8c6ac8eaacf1c535af6f241f2b7ff5cb3547ad55
George Talnagi <gtalnagi@yahoo.com>
2020-08-03 19:04:32 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -24,25 +25,32 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |key, value|
+    value << supply
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring].each do |key, value|
+    value << supply
+  end 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].collect do |key, value|
+  value
+  end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,17 +61,29 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |event, supplies|
+      puts "  #{event.to_s.split("_").collect { |a| a.capitalize }.join(" ") }: #{supplies.join(", ")}"
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.collect do |season, holidays|
+    holidays.collect do |event, supplies|
+      if supplies.include?("BBQ")
+        event 
+      end 
+    end
+  end.flatten.compact 
 
 end
 
 
-
+#followed along with the youtube walkthrough for the last 2, could not figure them out 
 
 
 
